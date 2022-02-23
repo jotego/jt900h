741d153a43b14b3ac7e6e30586028660e75dbb5c
Jose Tejada <jotego@gmail.com>
2022-02-23 09:06:10 +0100

    More LDDR tests

---
+++ ver/top/tests/lddr4.asm
@@ -0,0 +1,43 @@
+    ; LDDR4, uses word transfers with XIX,XIY pair
+    main section code
+    org 0
+    ld a,0xbf    ; common header
+
+    ld xiy,data+3*2
+    ld xix,copy+3*2
+    ld bc,4
+
+    lddrw (xix-),(xiy-)
+    jp ov,bad_end
+
+    cp bc,0
+    jp ne,bad_end
+
+    ; check the copy
+    ld bc,4
+    ld xhl,data+3*2
+    ld xde,copy+3*2
+loop:
+    ld wa,(xhl)
+    sub xhl,2
+    cpd wa,(xde-)
+    jp ne,bad_end
+    cp bc,0
+    jp ne,loop
+
+test_end:
+    ; ld (0xffff),0xff
+end_loop:
+    ldf 0
+    ld hl,0xbabe
+    jp end_loop
+bad_end:
+    ld hl,0xdead
+    jp bad_end
+    ;db 0
+data:
+    dw 0x0011,0x2233,0x4455,0x6677,0x8899,0x0000,0x0000,0x0000
+    dw 0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
+copy:
+    dw 0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000
+    end
