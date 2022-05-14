9d81b9e3605adb26a4a4e80d17c1365865dd217a
Jose Tejada <jotego@gmail.com>
2022-05-14 20:26:47 +0200

    LD<W> (#8),#

---
+++ ver/top/tests/ld_imm2mx8.asm
@@ -0,0 +1,35 @@
+    ; ld<w> (#8),#
+    main section code
+    org 0
+    ld a,0xbf    ; common header
+
+    ld xix,0xf0
+
+    ; 16-bit write
+    ldw (0xf0),0x1234
+    ld wa,(xix)
+    cp wa,0x1234
+    jp ne,bad_end
+    or ra3,1
+
+    ; 8-bit write
+    ldb (0xf0),0xba
+    ld b,(xix)
+    cp b,0xba
+    jp ne,bad_end
+    or ra3,2
+
+end_loop:
+    ldf 0
+    ld hl,0xbabe
+    ld (0xffff),0xff
+    jp end_loop
+bad_end:
+    ld hl,0xdead
+    ld (0xffff),0xff
+    jp bad_end
+data:
+    dw 0xcafe,0xbeef,0x1234,0x5678,0x9abc
+data2:
+    dw 0x0000,0x0000,0x0000,0x0000,0x0000
+    end
