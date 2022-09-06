cbc8e07451af53cbe1d5a4e80d1f8d2cb772123b
Samuel Paracuellos <samuel.paracuellos.gmail>
2022-09-06 15:59:17 +0200

    Covered merge run at the end of runall.sh

--- ver/top/runall.sh
+++ ver/top/runall.sh
@@ -15,6 +15,12 @@ FAIL=$(grep FAIL runall.log | wc -l)
 
 if [ $FAIL = 0 ]; then
     figlet PASS
+    # Merge coverage results
+    if which covered >/dev/null; then
+        covered merge -o merged.cdd tests/*.cdd
+    else
+        echo "Install covered to run coverage sims"
+    fi 
 else
     echo Some tests failed:
     grep FAIL runall.log
