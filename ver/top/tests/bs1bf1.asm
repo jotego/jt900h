07f737f07f40a83fb2cdcf605cfc589012f55562
Jose Tejada <jose.tejada@jotego.es>
2023-12-25 20:50:13 +0100

    bs1bf1

--- ver/top/tests/bs1bf1.asm
+++ ver/top/tests/bs1bf1.asm
@@ -11,7 +11,7 @@
     bs1f a,bc
     jp nov,bad_end
 
-    ld bc,0x4000 ; bit 14 are set
+    ld bc,0x4000 ; bit 14 set
     bs1b a,bc
     sub a,14
     jp ne,bad_end
@@ -19,12 +19,23 @@
     sub a,14
     jp ne,bad_end
 
-    ld bc,0x8000 ; bit 15 are set
+    ld bc,0x8000 ; bit 15 set
     bs1f a,bc
     sub a,15
     jp ne,bad_end
     bs1b a,bc
     sub a,15
+    jp ne,bad_end
+
+    ld de,0x1008
+    bs1f a,de
+    jp ov,bad_end
+    cp a,3
+    jp ne,bad_end
+
+    bs1b a,de
+    jp ov,bad_end
+    cp a,12
     jp ne,bad_end
 
     include finish.inc
