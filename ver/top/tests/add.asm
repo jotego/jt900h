ddd4d5121299b355b9e236c4a6642e648194f371
Jose Tejada <jose.tejada@jotego.es>
2023-12-29 18:16:14 +0100

    wip

--- ver/top/tests/add.asm
+++ ver/top/tests/add.asm
@@ -2,35 +2,55 @@
     maxmode on
     relaxed on
     org 0
-    ld a,0xbf    ; common header
     ; 8-bit add and carry
+    ld qh,1
     ld  xwa,1   ; xwa = 1
     ld  w,1     ; w = 1
     add w,a     ; w = 2
+    jp c,bad_end
+    jp ov,bad_end
+
+    ld qh,2
     ld  e,0xff  ; e = 0xff
     add e,a     ; e is set to zero, carry is set to 1
+    jp nc,bad_end
+    jp ov,bad_end
+
+    ld qh,3
     adc d,e     ; d = 1
     ld  l,0x38  ; l = 38
     ld  h,0x1a  ; h = 1a
     add h,l     ; h = 52
+    jp mi,bad_end
+    jp c,bad_end
+    jp ov,bad_end
+
     ; 16-bit adds
+    ld qh,4
     incf            ; bank 1
     ld wa,0x3456    ; wa = 3456
-    ld de,0xf000    ; de = f000
-    add wa,de       ; wa = 2426 + carry
+    ld de,0x8000    ; de = 8000
+    add wa,de       ; wa = 2426
+    jp pl,bad_end
+    jp c,bad_end
+
+    ld qh,5
+    ld h,0
+    scf
     adc h,h         ; h = 1
+    jp z,bad_end
+    jp c,bad_end
+
     ;; 32-bit adds
+    ld qh,6
     incf
-    ld xwa,0x10000000
-    ld xde,0x0FFFFFFF
+    ld xwa,0x01000000
+    ld xde,0x7FFFFFFF
     add xwa,xde
-    nop
-    nop
-    nop
-    nop
-test_end:
-    ld (0xffff),0xff
-    jp test_end
+    jp nov,bad_end
+    jp c,bad_end
+
+    include finish.inc
 data:
     dw 0xcafe,0xbeef,0xffff,0xeeee,0xcccc
     end
--- ver/top/tests/ex2.asm
+++ ver/top/tests/ex2.asm
@@ -9,37 +9,31 @@
     ex (xix),a
     cp a,0xfe
     jp ne,bad_end
-    or ra3,1
+    cp (xix),0x34
+    jp ne,bad_end
 
+    ld qh,1
     ld b,(xix)
     cp (xix),0x34
     jp ne,bad_end
-    or ra3,2
 
+    ld qh,2
     incf
     ld bc,(xix+2)
     ld de,0xfea5
 
+    ld qh,3
     ld wa,de
     ex (xix+2),wa
     cp wa,bc
     jp ne,bad_end
-    or ra3,4
 
+    ld qh,4
     ld wa,(xix+2)
     cp wa,de
     jp ne,bad_end
-    or ra3,8
 
-end_loop:
-    ldf 0
-    ld hl,0xbabe
-    ld (0xffff),0xff
-    jp end_loop
-bad_end:
-    ld hl,0xdead
-    ld (0xffff),0xff
-    jp bad_end
+    include finish.inc
 data:
     dw 0xcafe,0xbeef,0x1234,0x5678,0x9abc
     end
